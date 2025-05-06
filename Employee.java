4cede086bd26001bcea162ffe2a3a71caba85208
vinu033 <vinaykalasannavar124@gmail.com>
2025-05-06 14:37:03 +0530

    save file

---
+++ Employee.java
@@ -0,0 +1,21 @@
+public class Employee {
+int id;
+double salary;
+public Employee (String name,int id,double salary)
+{
+    this.name=name;
+    this.id=id;
+    this.salary=salary;
+
+}
+public void displayinfo(){
+    System.out.println("Employee Id : "+id);
+    System.out.println("Employee Name : "+name);
+    System.out.println("Employee Salary : "+salary);
+
+}
+public static void main(String[] args) {
+    Employee emp1=new Employee("xyz",101,55000.0);
+    emp1.displayinfo();
+}
+}
