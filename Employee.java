b5d1910f68e5f80edd90b36def205de9b67df855
vinu033 <vinaykalasannavar124@gmail.com>
2025-05-06 16:09:14 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -12,16 +12,12 @@ public Employee (String name,int id,double salary)
 public void displayinfo(){
     System.out.println("Employee Id : "+id);
     System.out.println("Employee Name : "+name);
-    System.out.println("Employee Salary : "+salary);
-    System.out.println("Employee phone no");
+    System.out.println("Employee Salary : no"+salary);
 
 }
 public static void main(String[] args) {
     Employee emp1=new Employee("xyz",101,55000.0);
-    Employee emp=new Employee("abc",102,65000.0);
 
     emp1.displayinfo();
-    emp.displayinfo();
-
 }
-}
+} 
