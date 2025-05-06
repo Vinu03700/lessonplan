3f16717d483d5d15d7b3094a577439fd1c7b7253
vinu033 <vinaykalasannavar124@gmail.com>
2025-05-06 16:29:40 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,28 +1,24 @@
 public class Employee {
     String name;
-int id;
-double salary;
-        String adress;
+    int id;
+     double salary;
+      
     
-public Employee (String name,int id,double salary, String adress)
+public Employee (String name,int id,double salary,  )
 {
     this.name=name;
     this.id=id;
     this.salary=salary;
-       this.adress=adress;
 
 }
 public void displayinfo(){
     System.out.println("Employee Id : "+id);
     System.out.println("Employee Name : "+name);
     System.out.println("Employee Salary : no"+salary);
-    System.out.println("Employee adress:" +adress);
 
 }
 public static void main(String[] args) {
-    Employee emp1=new Employee("xyz",101,55000.0,"Hubbali");
-
-
+    Employee emp1=new Employee("xyz",101,55000.0);
     emp1.displayinfo();
 
 }
